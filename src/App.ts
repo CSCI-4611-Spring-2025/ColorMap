6616d9f1bc63939d3910dc72fc768f2e1165c536
Dan Orban <dtorban@umn.edu>
2025-02-27 06:56:49 -0600

    Base code.

--- src/App.ts
+++ src/App.ts
@@ -7,23 +7,16 @@
 
 import * as gfx from 'gophergfx'
 import { GUI } from 'dat.gui'
-import { Earth } from './Earth';
-import { EarthquakeDatabase } from './EarthquakeDatabase';
 
 export class App extends gfx.GfxApp
 {
-    private earth: Earth;
-    private earthquakeDB: EarthquakeDatabase;
 
     // State variables
     private currentTime: number;
 
     // GUI variables
     public gui: GUI;
-    public date: string;
     public viewMode: string;
-    public meshResolution: number;
-    public playbackSpeed: number;
     public displayMode: string;
 
 
@@ -33,20 +26,10 @@ export class App extends gfx.GfxApp
         // initialize the base class gfx.GfxApp
         super();
 
-        this.earth = new Earth();
-        this.earthquakeDB = new EarthquakeDatabase('./assets/earthquakes.txt');
-
         this.currentTime = Infinity;
 
         this.gui = new GUI();
-        this.date = '';
         this.viewMode = 'Map';
-
-        // This is intentionally set very low to start to encourage debugging with a small 
-        // number of triangles. Once you get things working, a good default value might be 20.
-        this.meshResolution = 5;
-
-        this.playbackSpeed = 0.5;
 
         this.displayMode = 'Textured';
     }
@@ -71,37 +54,18 @@ export class App extends gfx.GfxApp
         background.layer = 1;
         this.scene.add(background);
 
-        // Create the earth mesh and add it to the scene
-        this.earth.initialize();
-        this.earth.createMesh(this.meshResolution);
-        this.scene.add(this.earth);
-
         // Create a new GUI folder to hold earthquake controls
-        const controls = this.gui.addFolder('Earthquake Controls');
-
-        // Create a GUI control to show the current date and make it listen for changes
-        const dateController = controls.add(this, 'date');
-        dateController.name('Current Date');
-        dateController.listen();
+        const controls = this.gui.addFolder('Controls');
 
         // Create a GUI control for the view mode and add a change event handler
         const viewController = controls.add(this, 'viewMode', {Map: 'Map', Globe: 'Globe'});
         viewController.name('View Mode');
-        viewController.onChange((value: string) => { this.earth.globeMode = value == 'Globe' });
-
-        // Create a GUI control for the playback speed and add a change event handler
-        const meshResolutionController = controls.add(this, 'meshResolution', 4, 100, 4);
-        meshResolutionController.name('Mesh Resolution');
-        meshResolutionController.onChange((value: number) => { this.earth.createMesh(value) });
-        
-        // Create a GUI control for the playback speed and add a change event handler
-        const playbackController = controls.add(this, 'playbackSpeed', 0, 10);
-        playbackController.name('Playback Speed');
+        viewController.onChange((value: string) => { /*this.earth.globeMode = value == 'Globe'*/ });
 
         // Create a GUI control for the debug mode and add a change event handler
         const debugController = controls.add(this, 'displayMode', ['Textured', 'Wireframe', 'Vertices']);
         debugController.name('Display Mode');
-        debugController.onChange((value: string) => { this.earth.changeDisplayMode(value) });
+        debugController.onChange((value: string) => { /*this.earth.changeDisplayMode(value)*/ });
 
         // Make the GUI controls wider and open by default
         this.gui.width = 300;
@@ -112,35 +76,6 @@ export class App extends gfx.GfxApp
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
-        // Scale factor for time progression
-        const playbackScale = 30000000000;
-
-        // Advance current time in milliseconds
-        this.currentTime += playbackScale * this.playbackSpeed * deltaTime;
-
-        // If we are beyond the max time, loop back to the beginning
-        if (this.currentTime > this.earthquakeDB.getMaxTime()) {
-            this.currentTime = this.earthquakeDB.getMinTime();
-            this.earthquakeDB.reset();
-        }
-
-        // Update the current date
-        const currentDate = new Date();
-        currentDate.setTime(this.currentTime);
-        this.date = currentDate.getUTCMonth() + "/" + currentDate.getUTCDate() + "/" + currentDate.getUTCFullYear();
-
-        // Create earthquake markers for each record in the quake database
-        let quake = this.earthquakeDB.getNextQuake(currentDate);
-        while (quake) {
-            this.earth.createEarthquake(quake);
-            quake = this.earthquakeDB.getNextQuake(currentDate);
-        }
-
-        // Update the earth animations
-        this.earth.update(deltaTime);
-
-        // Animate the earthquakes and remove old ones
-        this.earth.animateEarthquakes(this.currentTime);
     }
 
 
@@ -148,7 +83,7 @@ export class App extends gfx.GfxApp
     onMouseMove(event: MouseEvent): void
     {
         if (event.buttons == 1) {
-            this.earth.mouseRotation.multiply(gfx.Quaternion.makeRotationX(event.movementY *  0.01));
+            //this.earth.mouseRotation.multiply(gfx.Quaternion.makeRotationX(event.movementY *  0.01));
         }
     }
 
